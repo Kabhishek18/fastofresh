a8d6ada0aa2fb49a7fc0c68dd12e1c3341107db5
KRAB <developer@kabhishek18.com>
2021-02-09 12:17:05 +0530

    chnage

--- resources/views/front/inc/header.blade.php
+++ resources/views/front/inc/header.blade.php
@@ -16,6 +16,15 @@
     <link rel="stylesheet" href="{{ url('assets/css/responsive.css') }}">
     <link rel="stylesheet" href="{{ url('assets/css/custom.css') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-188367381-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-188367381-1');
+</script>
 </head>
 <body itemscope>
     <main style="padding-top:108px; ">
