6b4c9ee86c36e995b7bd3a54ea86bf695787aee9
KRAB <developer@kabhishek18.com>
2021-02-12 10:07:26 +0530

    meta

--- resources/views/front/inc/header.blade.php
+++ resources/views/front/inc/header.blade.php
@@ -3,10 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-    <title>Order Meat Online - Buy Fresh & High Quality Meat at Best Price on Fast O Fresh</title>
+    <meta name="description" content="Fastofresh is one stop destination if you are looking for Chicken, Mutton, Fish, or something else. We deliver fresh and quality meat at your doorstep." />
+    <meta name="keywords" content="Fresh chicken, fresh mutton, fresh fish, online chicken, online mutton, online fish, non-veg delivery online" />
+    <title>Fastofresh – Buy Fresh Chicken, Mutton and Fish Online</title>
     
+    <meta name="Distribution" content="Global" />
+<meta name="GOOGLEBOT" content="INDEX, FOLLOW" />
+<meta name="language" content="en" />
+<meta name="HandheldFriendly" content="True" />
+<link rel="canonical" href="https://fastofresh.com"/>
+
+
+
 
     <link rel="stylesheet" href="{{ url('assets/css/icons.min.css') }}">
     <link rel="stylesheet" href="{{ url('assets/css/bootstrap.min.css') }}">
