413981271ebbcc979c3ae8ad5c917dc95a3f3d72
KRAB <developer@kabhishek18.com>
2021-03-03 13:50:39 +0530

    chnage

--- resources/views/front/inc/header.blade.php
+++ resources/views/front/inc/header.blade.php
@@ -10,16 +10,25 @@
   if($parameters ==null ) { ?>
     <!-- Homepage -->
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Fastofresh is one stop destination if you are looking for Chicken, Mutton, Fish, or something else. We deliver fresh and quality meat at your doorstep." />
+    <meta name="description" content="Fast O Fresh is one stop destination if you are looking for Chicken, Mutton, Fish, or something else. We deliver fresh and quality meat at your doorstep." />
     <meta name="keywords" content="Fresh chicken, fresh mutton, fresh fish, online chicken, online mutton, online fish, non-veg delivery online" />
     <meta name="GOOGLEBOT" content="INDEX, FOLLOW" />
     <link rel="canonical" href="https://fastofresh.com"/>
-    <title>Fastofresh – Buy Fresh Chicken, Mutton and Fish Online</title>
+    <title>Fast O Fresh – Buy Fresh Chicken, Mutton and Fish Online</title>
     <meta name="p:domain_verify" content="d01ea80fe2ed4696baf58ae4a5c05eb5"/>
+    <!-- DO NOT MODIFY -->
+    <!-- Quora Pixel Code (JS Helper) -->
+    <script>
+    !function(q,e,v,n,t,s){if(q.qp) return; n=q.qp=function(){n.qp?n.qp.apply(n,arguments):n.queue.push(arguments);}; n.queue=[];t=document.createElement(e);t.async=!0;t.src=v; s=document.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s);}(window, 'script', 'https://a.quora.com/qevents.js');
+    qp('init', '5303ce752b6a4d279a758a39e95d6bc8');
+    qp('track', 'ViewContent');
+    </script>
+    <noscript><img height="1" width="1" style="display:none" src="https://q.quora.com/_/ad/5303ce752b6a4d279a758a39e95d6bc8/pixel?tag=ViewContent&noscript=1"/></noscript>
+    <!-- End of Quora Pixel Code -->
 
 <?php }elseif($parameters =='about-us'){ ?>
   <!-- About Us -->
-   <title>About Us | Fastofresh</title>
+   <title>About Us | Fast O Fresh</title>
   <meta name="description" content='Fast O Fresh is most effortless approach to arrange great crude meat and seafood.'/>
   <meta name="keywords" content="chicken, mutton, seafoods, fish, seafood, momos" />
 
@@ -34,25 +43,39 @@
  <!-- Why us -->
 <?php }elseif($parameters =='terms'){ ?>
   <!--terms  -->
-<title>Terms & Condition | Fastofresh</title>
+<title>Terms & Condition | Fast O Fresh</title>
 <meta name="description" content="" />
 <meta name="keywords" content=" " />
 
  <!-- terms --> 
 <?php }elseif($parameters =='faq'){ ?>
   <!--faq  -->
- <title>Faqs | Fastofresh</title>
+ <title>Faqs | Fast O Fresh</title>
 <meta name="description" content="" />
 <meta name="keywords" content=" " />
 
  <!-- faq --> 
  <?php }elseif($parameters =='privacyandpolicy'){ ?>
-  <!--terms  -->
- <title>Privacy and Policy | Fastofresh</title>
+  <!--privacyandpolicy  -->
+ <title>Privacy and Policy | Fast O Fresh</title>
 <meta name="description" content="" />
 <meta name="keywords" content=" " />
 
- <!-- terms -->  
+ <!-- privacyandpolicy --> 
+
+<?php }elseif($parameters =='cart'){ ?>
+ <title>Cart | Fast O Fresh</title>
+<meta name="description" content="" />
+<meta name="keywords" content=" " />
+  <!-- Quora Pixel Code (JS Helper) -->
+    <script>
+    !function(q,e,v,n,t,s){if(q.qp) return; n=q.qp=function(){n.qp?n.qp.apply(n,arguments):n.queue.push(arguments);}; n.queue=[];t=document.createElement(e);t.async=!0;t.src=v; s=document.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s);}(window, 'script', 'https://a.quora.com/qevents.js');
+    qp('init', '5303ce752b6a4d279a758a39e95d6bc8');
+    qp('track', 'ViewContent');
+    </script>
+    <noscript><img height="1" width="1" style="display:none" src="https://q.quora.com/_/ad/5303ce752b6a4d279a758a39e95d6bc8/pixel?tag=ViewContent&noscript=1"/></noscript>
+    <!-- End of Quora Pixel Code -->
+ <!-- terms -->   
 <?php }elseif($parameters =='category'){ ?>
   <!-- Category -->
       {!!$category->meta!!}
