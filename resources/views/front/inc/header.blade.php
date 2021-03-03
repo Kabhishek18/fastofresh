03600fe68fa45a817da1ea1431a550c5278a0842
KRAB <developer@kabhishek18.com>
2021-03-03 15:52:34 +0530

    change

--- resources/views/front/inc/header.blade.php
+++ resources/views/front/inc/header.blade.php
@@ -110,6 +110,21 @@
     qp('track', 'ViewContent');
     </script>
     <noscript><img height="1" width="1" style="display:none" src="https://q.quora.com/_/ad/5303ce752b6a4d279a758a39e95d6bc8/pixel?tag=ViewContent&noscript=1"/></noscript>
+          <script>
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '3429351090431555');
+      fbq('track', 'PageView');
+      </script>
+      <noscript><img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=3429351090431555&ev=PageView&noscript=1"
+      /></noscript>
     <!-- End of Quora Pixel Code -->
  <!-- terms -->   
 <?php }elseif($parameters =='category'){ ?>
